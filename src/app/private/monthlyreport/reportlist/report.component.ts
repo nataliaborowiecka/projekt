db8daabbab0de9f69754bbd4e08e9cca7ea15191
Natalia <ntlbrwck@gmail.com>
2019-05-20 20:39:36 +0200

    .

--- src/app/private/monthlyreport/reportlist/report.component.ts
+++ src/app/private/monthlyreport/reportlist/report.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DailyreportAddExpensesComponent } from '../../dailyreport/add/add-expenses.component';
 
 @Component({
     selector: 'app-report-list',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
   export class ReportComponent  {
     displayedColumns: string[] = ['id', 'notes', 'expenses', 'service', 'action'];
     dataSource = [];
+    @Input() expenses:DailyreportAddExpensesComponent[]
   }  
