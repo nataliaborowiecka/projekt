761484ab26a82a71cfd90f9a30e4ba67175a5f4a
michak <michal@surprise.design>
2019-07-05 14:16:34 +0200

    PathValue

--- src/app/private/employee/add/employee-add.component.ts
+++ src/app/private/employee/add/employee-add.component.ts
@@ -2,31 +2,31 @@ import { FormGroup } from '@angular/forms';
 import { EmployeesService } from '../employee.service';
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router'
-import {Employee} from '../employee';
+import { Router } from '@angular/router';
+import { Employee } from '../employee';
 import { EmployeeFormBuilder } from './employee-add-form.builder';
 @Component({
   selector: 'app-employee-add',
-  templateUrl: './employee-add.component.html',
+  templateUrl: '../form/form.component.html',
   styleUrls: ['./employee-add.employee.scss']
 })
 export class EmployeeAddComponent {
-  employee: FormGroup = EmployeeFormBuilder.build()
+  employee: FormGroup = EmployeeFormBuilder.build();
   confirmed = false;
 
-  constructor(private employeesService: EmployeesService,
+  constructor(
+    private employeesService: EmployeesService,
     private snackBar: MatSnackBar,
-    private router: Router) {}
-
+    private router: Router
+  ) {}
 
   save() {
-    console.log('dodano',)
+    console.log('dodano');
     this.employeesService.add(this.employee.value).subscribe(response => {
       this.snackBar.open('Poprawnie dodano pracownika', 'Zamknij', {
-        duration: 2000,
+        duration: 2000
       });
       this.router.navigate(['/app/employee']);
-      
     });
   }
 }
--- src/app/private/employee/edit/edit.component.ts
+++ src/app/private/employee/edit/edit.component.ts
@@ -2,45 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { EmployeesService } from '../employee.service';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
+import { EmployeeFormBuilder } from '../add/employee-add-form.builder';
 
 @Component({
   selector: 'app-edit',
-  templateUrl: '../add/employee-add.component.html',
+  templateUrl: '../form/form.component.html',
   styleUrls: ['./edit.component.scss']
 })
 export class EmployeeEditComponent implements OnInit {
   id = '';
-  employee = {
-    name: '',
-    surname: ''
-  };
 
-  constructor(private acRouter: ActivatedRoute,
+  employee: FormGroup = EmployeeFormBuilder.build();
+
+  constructor(
+    private acRouter: ActivatedRoute,
     private employeesService: EmployeesService,
     private snackBar: MatSnackBar,
-    private router: Router) {
+    private router: Router
+  ) {
     this.id = this.acRouter.snapshot.params.id;
   }
 
   ngOnInit() {
-    this.employeesService.getOneById(this.id)
-      .subscribe(
-        (response: any) => {
-          this.employee = response;
-        }
-      );
+    this.employeesService.getOneById(this.id).subscribe((response: any) => {
+      this.employee.patchValue(response);
+    });
   }
 
   save() {
-    this.employeesService.update(this.employee, this.id)
-      .subscribe(
-        (response) => {
-          this.snackBar.open('Poprawnie edytowano pracownika', 'Zamknij', {
-            duration: 2000,
-          });
-          this.router.navigate(['/app/employee']);
-        }
-      )
+    this.employeesService.update(this.employee.value, this.id).subscribe(response => {
+      this.snackBar.open('Poprawnie edytowano pracownika', 'Zamknij', {
+        duration: 2000
+      });
+      this.router.navigate(['/app/employee']);
+    });
   }
 }
